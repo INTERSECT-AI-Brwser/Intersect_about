93d9213d26a0c7c20aec68c1675d7b0805fb8359
Abhas <oliabhas2001@gmail.com>
2024-12-02 00:17:10 -0600

    Final Browser Version without Resizing Component

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+const root = createRoot(document.getElementById("root"));
+
+
+root.render(
+        <App/>
+);
